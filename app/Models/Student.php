c7a8e2c58f0d0466361852efad48212cc14a7b49
arilybaert <arilybae@arteveldehs.be>
2020-10-29 23:28:54 +0100

    linked yearbook to db

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
 
 class Student extends Model
 {
     use HasFactory;
+
+    // public function course()
+    // {
+    //     // return $this->belongsTo('App\Models\Course');
+    //     return $this->hasOne('App\Models\Course');
+    // }
+    public function course()
+
+    {
+        return $this->belongsTo('App\Models\Course');
+    }
 }
--- resources/views/pages/yearbook.blade.php
+++ resources/views/pages/yearbook.blade.php
@@ -3,7 +3,22 @@
 @section('content')
 <h1 class="a-page-title">Yearbook</h1>
 
+    <div class="o-yearbook-filter">
+        @foreach ($courses as $course)
+            <a href="{{ url('yearbook/'. $course->title_short .'/') }}">{{$course->title_short}}</a>
+
+        @endforeach
+
+    </div>
     <div class="o-yearbook-students container-fluid">
+
+        @foreach ($students as $student)
+            <div class="o-studentcards col-sm-12 col-md-4 col-lg-2 col-xl-2">
+                <img class="a-yearbook-profilepicture" src="{{ $student->picture}}" alt="">
+                <h2 class="a-yearbook-student-name">{{ $student->first_name . ' ' . $student->last_name }}</h2>
+                <p class="a-yearbook-student-major">{{ $student->course->title}}</p>
+            </div>
+        @endforeach
         <div class="o-studentcards col-sm-12 col-md-4 col-lg-2 col-xl-2">
             <img class="a-yearbook-profilepicture" src="https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=face&w=2000&h=1333&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2019%2F10%2Fmark-zuckerberg-1-2000.jpg" alt="">
             <h2 class="a-yearbook-student-name">Mark Zuckerberg</h2>
