c6d0cbe029a76d752fc1c0c9ca91f7bec8762558
arilybaert <arilybae@arteveldehs.be>
2020-11-11 18:49:19 +0100

    updated and styled the homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,15 +1,21 @@
 @extends('layout')
 
 @section('content')
-<div class="o-home">
-    @foreach ( $courses as $course )
-        <a class="o-home-card" href="{{ route('portfolio-cmo')}}">
-            <img src="{{ $course->img_url}}" alt="" class="a-home-card-image">
-            <h2 class="a-home-card-title">{{ $course->title_short}}</h2>
-            <h3 class="a-home-card-subtitle">{{ $course->title}}</h3>
-            <p class="a-home-explination">{{ $course->content}}</p>
-        </a>
-    @endforeach
+<div class="container-md">
+    <div class="o-home row">
+        @foreach ( $courses as $course )
+            <a class="o-home-card col-12 col-sm-6 col-md-3" href="{{ route('portfolio-cmo')}}">
+                <div class="m-img-container">
+                    <img src="{{ $course->img_url}}" alt="" class="a-home-card-image">
+                    <div class="m-home-card-tag a-bg-color-{{ $course->title_short}}">
+                        <span class="a-home-card-tag ">{{ $course->title_short}}</span>
+                    </div>
+                </div>
+                <h3 class="a-home-card-subtitle">{{ $course->title}}</h3>
+                <p class="a-home-explination">{{ $course->content}}</p>
+            </a>
+        @endforeach
+    </div>
 </div>
 
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,2 +1,7 @@
+<footer class="o-footer row">
+    <div class="col-12 col-sm-3 offset-sm-7 a-footer-img">
+        <img src="/src/img/logo/ahs.png" alt="">
+    </div>
+</footer>
 </body>
 </html>
