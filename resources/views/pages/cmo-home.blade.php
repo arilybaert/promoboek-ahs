d6198b5829163a7596711aef24ee12dcd98724c1
arilybaert <arilybae@arteveldehs.be>
2020-11-19 16:16:42 +0100

    started with making cmo-portfolio clickable

--- resources/views/pages/cmo-home.blade.php
+++ resources/views/pages/cmo-home.blade.php
@@ -13,23 +13,45 @@
 
         @foreach ( $students as $student)
 
-            <?php $col_size = $col_size+1; ?>
             <?php
+                // check thumbnail aspect ratio to give according style class
 
-            // check thumbnail aspect ratio to give according style class
+                $image_size_array = getimagesize($student->thumbnail);
 
-            $image_size_array = getimagesize($student->thumbnail);
+                $image_width = $image_size_array[0];
+                $image_height = $image_size_array[1];
+                $image_aspect_ratio = ($image_size_array[0] / $image_size_array[1]);
+            ?>
 
-            $image_width = $image_size_array[0];
-            $image_height = $image_size_array[1];
-            $image_aspect_ratio = ($image_size_array[0] / $image_size_array[1]);
-?>
+            <span class="
+                {{ $image_aspect_ratio >= 1 ? 'm-landscape' : 'm-portait' }} m-cmo-img trigger">
 
-{{-- href="/portfolio-cmo/rick" --}}
-            <span  class="{{ $image_aspect_ratio >= 1 ? 'm-landscape' : 'm-portait' }}    m-cmo-img">
-                <img src="{{ $student->thumbnail}}" alt="" class="a-cmo-image">
-
+                <img src="{{ $student->thumbnail }}" alt="" class="a-cmo-image">
             </span>
+
+            <!-- The Modal -->
+            <div class="a-modal">
+
+                <!-- Modal content -->
+                <div class="o-modal-content">
+                    <span class="close">&times;</span>
+
+                        <div class="row">
+                            <div class="col-5">
+                                <img src="{{ $student->thumbnail }}" alt="" class="a-cmo-modal-image">
+                            </div>
+                            <div class="o-student-portofolio-info col-6">
+                                <h2>Titel werk</h2>
+                                <h3>{{$student->first_name . " " . $student->last_name}}</h3>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, qu
+
+                                </p>
+                            </div>
+                    </div>
+                </div>
+
+            </div>
 
 
         @endforeach
