4bd0ee2225a01ec7065e9bc1f1b5fb1bc552fd37
arilybaert <arilybae@arteveldehs.be>
2020-11-30 18:48:35 +0100

    made lighthouse api

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="o-footer row">
     <div class="col-12 col-sm-3 offset-sm-7 a-footer-img">
-        <img src="src/img/logo/ahs.png" alt="">
+        <img src="{{ asset('src/img/logo/ahs.png')}}" alt="">
 
     </div>
 </footer>
