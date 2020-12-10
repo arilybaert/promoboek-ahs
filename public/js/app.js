8e1c77318773ad432d547ce2f7e00f2c59c96e96
Jesse Vander <jessvand10@student.arteveldehs.be>
2020-12-10 15:07:43 +0100

    test comment

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/arilybaert/Sites/promoboek-ahs/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/arilybaert/Sites/promoboek-ahs/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /Users/jessevander/artevelde/pgm-code/20-21/semester3/atwork3/promoboek-ahs/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/jessevander/artevelde/pgm-code/20-21/semester3/atwork3/promoboek-ahs/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
