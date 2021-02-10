a27bb2b22c9a3a67f4e825fa45929ce99e1b8bb3
arilybaert <arilybae@arteveldehs.be>
2021-02-10 22:20:01 +0100

    added modal on cmo-detail-page

--- resources/views/pages/cmo-detail.blade.php
+++ resources/views/pages/cmo-detail.blade.php
@@ -17,6 +17,8 @@
         {{-- bio --}}
         <p class="a-cmo-detail-bio">{{$student->bio}}</p>
     </div>
+
+    {{-- portfolio images --}}
     <div class="o-cmo-detail-main">
 
         @foreach ($images as $image)
@@ -31,10 +33,40 @@
         $image_height = $image_size_array[1];
         $image_aspect_ratio = ($image_size_array[0] / $image_size_array[1]);
     ?>
-        <span  class="{{ $image_aspect_ratio >= 1 ? 'm-landscape' : 'm-portait' }}">
+        <span  class="{{ $image_aspect_ratio >= 1 ? 'm-landscape' : 'm-portait' }} trigger">
             <img src="{{ asset($image->url) }}" alt="" class="a-cmo-detail-img">
 
         </span>
+                    <!-- The Modal -->
+                    <div class="a-modal">
+
+                        <!-- Modal content -->
+                        <div class="o-modal-content">
+
+
+                                <div class="row">
+                                    <div class="col-10">
+                                        <div class="row">
+                                            <div class="col-12 col-md-5">
+                                                <img src="{{ asset($image->url) }}" alt="" class="a-cmo-modal-image">
+                                            </div>
+                                            <div class="o-student-portofolio-info col-12 col-md-5">
+                                                <h2>{{$image->title}}</h2>
+                                                <h3>{{$student->first_name . " " . $student->last_name}}</h3>
+                                                <p class="a-cmo-portfolio-description">
+                                                    {{$image->content}}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-2 m-cmo-portfolioBtn">
+                                        <i class="fas fa-times close"></i>
+                                    </div>
+                            </div>
+                        </div>
+
+                    </div>
         @endforeach
     </div>
 
