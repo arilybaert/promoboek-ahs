0110450d6d73f2fa81d56ab80e60bc0e12ce7acf
arilybaert <arilybae@arteveldehs.be>
2021-03-08 15:14:51 +0100

    added some navlinks to student/admin backoffice + seperated changepassword-page for student and admin

--- resources/views/layouts/app-student.blade.php
+++ resources/views/layouts/app-student.blade.php
@@ -23,7 +23,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ route('student') }}">
                     {{-- {{ config('app.name', 'Laravel') }} --}}
                     Student backoffice
                 </a>
@@ -59,25 +59,24 @@
                                 </li>
                             @endif
                         @else
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('changePassword-student') }}">Change password</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('student') }}">Edit profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">Go back to site</a>
+                        </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('changePassword-form') }}">Change password</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a class="nav-link" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                </div>
                             </li>
                         @endguest
                     </ul>
